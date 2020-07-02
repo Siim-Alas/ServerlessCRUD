d739ae5f314774aa9473a035af12d01bf3efc0ca
Siim Alas <siim.alas@outlook.com>
2020-07-02 12:05:38 +0300

    Added fa icons stylesheet link and custom JS to implement a sticky navbar.

---
+++ ServerlessCrudBlazorUI/wwwroot/Javascript/OwnStickifier.js
@@ -0,0 +1,17 @@
+﻿// Workaround, since position: sticky; doesn't work with the overflow property set.
+
+window.OwnStickifier = {
+    stickify: function (element) {
+        let originalOffsetTop = element.offsetTop;
+        window.addEventListener("resize", function () {
+            originalOffsetTop = element.offsetTop;
+        });
+        document.addEventListener("scroll", function () {
+            if (window.pageYOffset > originalOffsetTop) {
+                element.classList.add("fixed-top");
+            } else {
+                element.classList.remove("fixed-top");
+            }
+        });
+    }
+}
