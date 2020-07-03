1a268daaaa41450cec3705e24eafbe48cca57195
Siim Alas <siim.alas@outlook.com>
2020-07-03 18:05:10 +0300

    Replaced pagination words with arrows and changed OwnStickifier.js to use the offsetHeight of an element containing all elements above the stickified element.

--- ServerlessCrudBlazorUI/wwwroot/Javascript/OwnStickifier.js
+++ ServerlessCrudBlazorUI/wwwroot/Javascript/OwnStickifier.js
@@ -1,15 +1,9 @@
 ﻿// Workaround, since position: sticky; doesn't work with the overflow property set.
 
 window.OwnStickifier = {
-    stickify: function (element) {
-        let originalOffsetTop = element.offsetTop;
-
-        window.addEventListener("resize", function () {
-            originalOffsetTop = element.offsetTop;
-        })
-
+    stickify: function (element, elementContainingAllAbove) {
         document.addEventListener("scroll", function () {
-            if (window.pageYOffset > originalOffsetTop) {
+            if (window.pageYOffset > elementContainingAllAbove.offsetHeight) {
                 element.classList.add("fixed-top");
             } else {
                 element.classList.remove("fixed-top");
