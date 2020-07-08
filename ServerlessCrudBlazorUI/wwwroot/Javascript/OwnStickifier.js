d60e2f9dc7b8a9d91a70282956b9515a53f6b11f
Siim Alas <siim.alas@outlook.com>
2020-07-08 16:13:42 +0300

    Fixed navbar stutter.

--- ServerlessCrudBlazorUI/wwwroot/Javascript/OwnStickifier.js
+++ ServerlessCrudBlazorUI/wwwroot/Javascript/OwnStickifier.js
@@ -1,12 +1,14 @@
 ﻿// Workaround, since position: sticky; doesn't work with the overflow property set.
 
 window.OwnStickifier = {
-    stickify: function (element, elementContainingAllAbove) {
+    stickify: function (element, elementContainingAllAbove, dummyElement) {
         document.addEventListener("scroll", function () {
             if (window.pageYOffset > elementContainingAllAbove.offsetHeight) {
+                dummyElement.style.height = `${element.offsetHeight}px`;
                 element.classList.add("fixed-top");
             } else {
                 element.classList.remove("fixed-top");
+                dummyElement.style.height = "0";
             }
         });
     }
